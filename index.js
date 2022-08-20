7a7f2fed4e42d8456e95393457d166b686879d1b
Gurban Gurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-08-20 16:14:06 +0400

    add

---
+++ index.js
@@ -0,0 +1,13 @@
+//Imports
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("erver");
+});
